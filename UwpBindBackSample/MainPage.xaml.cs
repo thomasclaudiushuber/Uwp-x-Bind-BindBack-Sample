d47415ee8e27622303a7b38becca1834fc5638ba
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2019-06-06 21:37:18 +0200

    Add project files.

---
+++ UwpBindBackSample/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace UwpBindBackSample
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private string _yesNoString;
+        public string YesNoString
+        {
+            get => _yesNoString;
+            set
+            {
+                if (_yesNoString != value)
+                {
+                    _yesNoString = value;
+                    this.Bindings.Update();
+                }
+            }
+        }
+
+        public bool GetYesNoValue()
+        {
+            return YesNoString == "yes";
+        }
+
+        public void SetYesNoValue(bool? val)
+        {
+            YesNoString = val == true ? "yes" : "no";
+        }
+    }
+}
